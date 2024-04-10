25d483d0644d1da4a2332b9013eb5095301482b4
Vinicius Solon <vini.97@live.com>
2024-04-09 21:12:59 -0300

    InitialÃ commit

---
+++ src/main/java/com/vinisolon/credit/proposal/application/SpringCreditProposalApplication.java
@@ -0,0 +1,13 @@
+package com.vinisolon.credit.proposal.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCreditProposalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCreditProposalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinisolon/credit/proposal/application/SpringCreditProposalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinisolon.credit.proposal.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCreditProposalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
